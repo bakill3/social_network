34508585d03afbe89c341a04b1a6e588319abafb
Gabriel Brandão <35882052+bakill3@users.noreply.github.com>
2018-09-23 22:41:20 +0100

    Update codigo.php

--- codigo.php
+++ codigo.php
@@ -61,11 +61,11 @@ if (isset($_POST['registo'])) {
 				$mail->isSMTP();                            // Set mailer to use SMTP
 				$mail->Host = 'smtp.gmail.com';              // Specify main and backup SMTP servers
 				$mail->SMTPAuth = true;                     // Enable SMTP authentication
-				$mail->Username = 'lifepageshop123@gmail.com'; // your email id
-				$mail->Password = 'flash136'; // your password
+				$mail->Username = 'YOUR EMAIL'; // your email id
+				$mail->Password = 'YOUR PASSWORD'; // your password
 				$mail->SMTPSecure = 'tls';                  
 				$mail->Port = 587;     //587 is used for Outgoing Mail (SMTP) Server.
-				$mail->setFrom('lifepageshop123@gmail.com', 'Sivex Social Network');
+				$mail->setFrom('YOUR EMAIL', 'Sivex Social Network');
 				$mail->addAddress($email);   // Add a recipient
 				$mail->isHTML(true);  // Set email format to HTML
 
